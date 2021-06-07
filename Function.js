9eaeef91e2fae65d781844043060fba06bbf7988
Ran500 <rayan-alfasl@outlook.sa>
2021-06-07 21:11:47 +0300

    All style Function

---
+++ Function.js
@@ -0,0 +1,61 @@
+/*
+ 
+
+                All Style For Function   
+
+*/
+
+/* [ =========================================================================================== ] */
+
+/* 
+   
+
+
+
+    Examples :. 
+
+
+*/
+
+
+
+function x(){
+    alert("Hello World! ");
+}
+
+
+
+
+function Num (Num1,Num2){
+
+    return  Num1 + Num2 ;
+}
+
+document.getElementById("Test").innerHTML =Num(10,10);
+
+
+
+
+
+
+function myFunction(a, b) {
+    return a * b;
+  }
+myFunction(10, 2);     
+
+
+
+
+
+
+
+
+
+
+// This Fun is Call Selfe and Started
+
+(function Num (){
+
+    alert("Hello World") ;
+ })();
+ 
